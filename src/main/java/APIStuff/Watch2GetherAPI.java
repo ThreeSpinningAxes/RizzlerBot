53991708f76c2f795b45d989231fda0b51cc48d7
Devraj Chudasama <59073691+ThreeSpinningAxes@users.noreply.github.com>
2022-11-16 22:52:15 -0500

    Update Watch2GetherAPI.java

--- src/main/java/APIStuff/Watch2GetherAPI.java
+++ src/main/java/APIStuff/Watch2GetherAPI.java
@@ -59,6 +59,6 @@ public class Watch2GetherAPI {
     }
     public static void main(String[] args) throws ConfigurationException, URISyntaxException, IOException, InterruptedException {
         Watch2GetherAPI a = new Watch2GetherAPI();
-        a.sendPOSTRequest("https://www.youtube.com/watch?v=sV2t3tW_JTQ&list=RDxMgE736TC4A&index=10");
+        a.sendPOSTRequest("");
     }
 }
